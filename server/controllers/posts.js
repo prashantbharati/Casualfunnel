e86b46c16090263c2c14f96266e54495a24963fd
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2023-01-26 20:30:51 +0530

    post route setup

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -3,8 +3,32 @@ import mongoose from "mongoose";
 
 const router = express.Router();
 
-export const getPosts = (req, res) => {
-  res.send("This works");
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPostMessage = new PostMessage({
+    ...post,
+    creator: req.userId,
+    createdAt: new Date().toISOString(),
+  });
+
+  try {
+    await newPostMessage.save();
+
+    res.status(201).json(newPostMessage);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
 
 export default router;
