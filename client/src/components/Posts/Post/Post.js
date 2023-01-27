f9cb9a4e3f637f029c14a571deae1db88d7565a6
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2023-01-27 14:55:03 +0530

    nav done

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core/";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined";
+
+import moment from "moment";
+
+import useStyles from "./styles";
+
+const Post = ({ post, setCurrentId }) => {
+  const classes = useStyles();
+  const user = JSON.parse(localStorage.getItem("profile"));
+
+  return (
+    <Card className={classes.card}>
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.name}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+      {(user?.result?.googleId === post?.creator ||
+        user?.result?._id === post?.creator) && (
+        <div className={classes.overlay2}>
+          <Button
+            onClick={() => setCurrentId(post._id)}
+            style={{ color: "white" }}
+            size="small"
+          >
+            <MoreHorizIcon fontSize="default" />
+          </Button>
+        </div>
+      )}
+
+      <Typography
+        className={classes.title}
+        gutterBottom
+        variant="h5"
+        component="h2"
+      >
+        {post.title}
+      </Typography>
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {post.message}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Post;
